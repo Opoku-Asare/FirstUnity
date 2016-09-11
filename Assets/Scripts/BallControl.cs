3072ebec524b2ca1ceeec6c41dc12a3ac62d8eab
Kennedy Opoku Asare <opokuasarekennedy@gmail.com>
2016-09-12 00:28:46 +0300

    First Commit

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour
+{
+
+    public float rotationSpead = 100.00f;
+    private Rigidbody rb;
+    public float jumpHeight = 8f;
+    private bool ballonned = false;
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+       // rotating a ball
+        var rotation = Input.GetAxis("Horizontal") * rotationSpead;
+        rotation *= Time.deltaTime;
+        rb.AddRelativeTorque(Vector3.back * rotation);
+
+        // jumping 
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0, jumpHeight, 0);
+           // ballonned = true;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    //break
+        //}
+      
+
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform target;
+    public float distance = -10f;
+    public float lift = 1.5f;
+    private GameObject ball;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+         * set Xcamera=Xball
+         * set Ycamera=Xball+1.5
+         * set Zcamera=-10
+         */
+        transform.position = target.position + new Vector3(0, lift, distance);
+        transform.LookAt(target);
+	
+	}
+}
